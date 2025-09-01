8e791f05e52c36c6b1ba594c3bf487fe7ce62380
Ali-Ashjaa <aliashjaa8@gmail.com>
2025-09-01 19:39:47 +0530

    fixed some bugs

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,6 +16,14 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
+
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({
+  limit: '50mb',
+  extended: true
+}));
+
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(
